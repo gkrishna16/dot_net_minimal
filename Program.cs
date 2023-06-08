586ed4efddf8edd3d84da163f371a0f39a5cacf7
Gopal Krishan <gkrishna160@gmail.com>
2023-06-08 01:31:23 -0400

    first function

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World Gopal, here is a new get route from your app.!");
+var books = new List<Book> {
+    new Book{Id = 1, Title = "The title is here.", Author= "The author is here."}
+};
+
+app.MapGet("/api", () => {
+    Console.WriteLine("The console application is runnning");
+    return books;
+});
+
+
 // getithere 
 app.Run();
+
+
+class Book
+{
+    public int Id { get; set; }
+    public required string Title{get; set;}
+    public required string Author { get; set; }
+
+}
